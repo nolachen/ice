6b2ed5090a395c2216f7e5a3664fa5657ae57700
Nola Chen <nolachen@Nolas-MacBook-Pro.local>
2019-03-21 19:09:24 +0800

    Init project

---
+++ ICE/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ICE project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ICE.settings')
+
+application = get_wsgi_application()
