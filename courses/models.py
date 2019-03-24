7f05942ae7b094f0ac2da245fcebb22535a74c77
allysonyoung <ayoung1009@gmail.com>
2019-03-24 14:19:42 +0800

    save method

--- courses/models.py
+++ courses/models.py
@@ -23,7 +23,21 @@ class Instructor(models.Model):
 
 # Component Model
 class Component(models.Component):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
     title = models.CharField(max_length=200)
     date_of_creation = models.DateField()
     date_of_last_update = models.DateField()
     index = models.IndexField()
+
+    class Meta:
+        ordering = ['index']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.index:
+            self.index = self.module.components.count() - 1
+        super(Component, self).save(*args, **kwargs)
+
