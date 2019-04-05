1308ef6638dec90a22c993bcab39d9c42531d072
Kenneth Chiu <kennethchiuhc@gmail.com>
2019-04-05 22:47:34 +0800

    Learner signup & activate using token & fill out username+password

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class SignupForm(forms.Form):
+class SignupForm(forms.ModelForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     #autobiography = forms.CharField(max_length=200)
     staff_id = forms.CharField(max_length=20)
@@ -10,3 +10,8 @@ class SignupForm(forms.Form):
         model = User
         #fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'autobiography')
         fields = ('staff_id', 'email')
+
+class LearnerRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import SignupForm
+from .forms import SignupForm, LearnerRegisterForm
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -26,10 +26,10 @@ def signup(request):
         if form.is_valid():
             user = form.save(commit=False)
             user.is_active = False
+            user.is_staff = True
             user.save()
             current_site = get_current_site(request)
             mail_subject = 'Activate your account.'
-            print('uid:', urlsafe_base64_encode(force_bytes(user.pk)))
             message = render_to_string('registration/acc_active_email.html', {
                 'user': user,
                 'domain': current_site.domain,
@@ -42,24 +42,42 @@ def signup(request):
             )
             email.send()
             return HttpResponse('Please confirm your email address to complete the registration')
+        else:
+            raise Http404
     else:
         form = SignupForm()
-    return render(request, 'registration/signup.html', {'form': form})
+    return render(request, 'registration/signup.html', {
+        'form': form
+    })
 
 def activate(request, uidb64, token):
-    print('uidb:', uidb64)
-    print('token:', token)
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
-        print('uid:', uid)
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        #user.is_active = True
+        #user.save()
+        return register_learner(request, user)
     else:
         return HttpResponse('Activation link is invalid!')
+
+def register_learner(request, user):
+    print('userid:', user.id)
+    if request.method=='POST':
+        form = LearnerRegisterForm(request.POST)
+        if form.is_valid():
+            print('username:', form.cleaned_data['username'])
+            user.username = form.cleaned_data['username']
+            user.set_password(form.cleaned_data['password2'])
+            user.is_active = True
+            user.save()
+        else:
+            raise Http404
+        return HttpResponse('Registered')
+    else:
+        form = LearnerRegisterForm()
+    return render(request, 'registration/register.html', {
+        'form': form
+    })
